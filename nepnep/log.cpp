70f911ed0a96a08f0101e10812623f39045e6d48
neilli <tasselleer@gmail.com>
2022-10-10 22:45:10 +0800

    test commite

---
+++ nepnep/log.cpp
@@ -0,0 +1,32 @@
+//
+// Created by lee on 2022/10/10.
+//
+
+#include <string>
+
+#pragma once
+namespace nepnep
+{
+    class Logger
+    {
+    public:
+        Logger();
+    private:
+    };
+
+    class LogAppender
+    {
+    public:
+        virtual ~LogAppender();
+
+    };
+
+    class LogFormatter
+    {
+
+    };
+
+
+
+
+}
